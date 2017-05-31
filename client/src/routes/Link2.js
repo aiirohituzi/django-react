4b13b6135c2d1df94928c4b3a4410d22a51c8ba5
aiirohituzi <sinn5656@gmail.com>
2017-05-31 17:19:14 +0900

    Add fileupload form.

--- client/src/routes/Link2.js
+++ client/src/routes/Link2.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
-const Link2 = () => {
-    return (
-        <div>
-            Link2
-        </div>
-    );
+class Link2 extends React.Component{
+    changeFile(e) {
+            this.setState({csvFile: e.target.files[0]});
+        }
+
+    importFilen() {
+            data = new FormData();
+            data.append('file', this.state.csvFile);
+            $.ajax({
+                type: "POST",
+                url: "/csv/import",
+                data: data,
+                dataType: "JSON"
+            }).done(function(json){
+                alert("hooray!");
+            });
+        }
+
+    render() {
+        return(
+            <div>
+                임시 작업용 페이지
+                <input type="file" onChange={this.changeFile}/>
+                <button onClick={this.importFile}>Import</button>
+            </div>
+        );
+    }
 };
 
 export default Link2;
