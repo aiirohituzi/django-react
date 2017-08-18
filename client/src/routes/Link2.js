368353f9a8f82f58ee2a48ef5a964eab2750de63
aiirohituzi <sinn5656@gmail.com>
2017-08-18 23:33:33 +0900

    Vue.js test in reactjs....failed

--- client/src/routes/Link2.js
+++ client/src/routes/Link2.js
@@ -9,82 +9,15 @@ class Link2 extends React.Component{
         super(props);
 
         this.state = {
-            imgInfo: '',
             list: [],
             loginId: null,
-            loginPw: null
+            loginPw: null,
+            postInfo: null,
         }
 
 
         this.test = this.test.bind(this);
-        this.getImage = this.getImage.bind(this);
         this.stateArrayTest = this.stateArrayTest.bind(this);
-        this.searchTest = this.searchTest.bind(this);
-    }
-
-
-    searchTest = async () => {
-        var data = new FormData();
-
-        data.append('category', 'all')
-        data.append('keyword', 'test');
-
-        const config = {
-            headers: { 'content-type': 'application/x-www-form-urlencoded' }
-        }
-
-        await axios.post('http://127.0.0.1:8000/search/', data, config)
-        .then(function (response) {
-            console.log(response.data)
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-        // console.log(axios.get('http://127.0.0.1:8000/posting/'));
-    }
-    
-
-    test = async () => {
-        var data = new FormData();
-
-        var image = document.getElementById('formControlsImage').files;
-        data.append('image', image);
-
-        const config = {
-            headers: { 'content-type': 'multipart/form-data' }
-        }
-
-        await axios.post('http://127.0.0.1:8000/test/', data, config)
-        .then(function (response) {
-            console.log('ㅇㅇㅇㅇㅇ')
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-    }
-
-    getImage = async () => {
-        var data = new FormData();
-        var img;
-        data.append('postId', '27');
-
-        const config = {
-            headers: { 'content-type': 'application/json' }
-        }
-
-        await axios.post('http://127.0.0.1:8000/images/', data, config)
-        .then(function (response) {
-            console.log(response);
-            img = response.data;
-        })
-        .catch(function (error) {
-            console.log(error);
-        });
-
-        this.setState({
-            imgInfo: img
-        });
     }
 
     stateArrayTest() {
@@ -99,7 +32,6 @@ class Link2 extends React.Component{
     }
 
     render() {
-        const imgPath = 'data:image/png;base64,' + this.state.imgInfo;
         const arr = this.state.list;
         
         const arrInstance = (
@@ -108,27 +40,12 @@ class Link2 extends React.Component{
             {arr[1]}<br/>
             {arr.length}
             </div>
-        )
-
-        const imgInstance = (
-            <img src={imgPath} width="80%" />
         );
+
         return(
             <div>
                 임시 작업용 페이지
 
-                <Button onClick={ this.searchTest }>searchtest</Button>
-                
-                <hr />
-                <FormGroup controlId="formControlsImage">
-                    <ControlLabel>이미지 업로드</ControlLabel>
-                    <FormControl type="file" multiple />
-                </FormGroup>
-                <Button bsClass="btn btn-primary" onClick={ this.test.bind(this) }>test</Button>
-                <hr />
-                <button onClick={this.getImage}>getImageTest</button>
-                {imgInstance}
-                <img src={require("file-loader?name=[sha512:hash:base64:7].[ext]!../image/2017/07/12/orig/test_image.png")} />
                 <button onClick={this.stateArrayTest}>state array test</button>
                 {arrInstance}
             </div>
