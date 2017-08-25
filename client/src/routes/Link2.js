36c2a1dc8622922483312b9a6e62c5d198442903
aiirohituzi <sinn5656@gmail.com>
2017-08-25 23:45:33 +0900

    Display received data in a list group.

--- client/src/routes/Link2.js
+++ client/src/routes/Link2.js
@@ -4,7 +4,7 @@ import update from 'react-addons-update'
 
 import React from 'react';
 
-import { Grid, Row, Col, FormGroup, ControlLabel, FormControl, Button, Form } from 'react-bootstrap';
+import { Grid, Button, Form, ListGroup, ListGroupItem } from 'react-bootstrap';
 
 
 class Link2 extends React.Component{
@@ -12,25 +12,12 @@ class Link2 extends React.Component{
         super(props);
 
         this.state = {
-            list: [],
             loginId: null,
             loginPw: null,
             tweetList: null,
         }
 
-        this.stateArrayTest = this.stateArrayTest.bind(this);
         this.getTweet = this.getTweet.bind(this);
-    }
-
-    stateArrayTest() {
-        this.setState({
-            list: update(
-                this.state.list, 
-                {
-                    $push: ['1', '2', '3']
-                }
-            )
-        });
     }
 
     getTweet = async () => {
@@ -42,7 +29,7 @@ class Link2 extends React.Component{
             if(!(response.data == 'False')){
                 // console.log(response.data.length);
                 for(var i=0; i<response.data.length; i++){
-                    console.log(i + ' : ' + response.data[i])
+                    // console.log(i + ' : ' + response.data[i])
                     tweets.push(response.data[i])
                 }
                 console.log(response)
@@ -61,30 +48,17 @@ class Link2 extends React.Component{
     }
 
     render() {
-        const arr = this.state.list;
         const tweetList = this.state.tweetList
 
-        console.log(tweetList);
-        
-        const arrInstance = (
-            <div>
-            {arr[0]}<br/>
-            {arr[1]}<br/>
-            {arr[2]}<br/>
-            {arr.length}
-            </div>
-        );
-
         const tweetListInstance = [];
-
         if(tweetList != null){
-            tweetListInstance.push(
-                <div>
-                {tweetList[0]}
-                {tweetList[1]}
-                {tweetList[2]}
-                </div>
-            )
+            for(var i=0; i<tweetList.length; i++){
+                tweetListInstance.push(
+                    <ListGroupItem>
+                        {tweetList[i]}
+                    </ListGroupItem>
+                );
+            }
         } else {
             tweetListInstance.push(
                 <div>
@@ -94,15 +68,12 @@ class Link2 extends React.Component{
         }
 
         return(
-            <div>
-                임시 작업용 페이지
-
-                <button onClick={this.stateArrayTest}>state array test</button>
-                {arrInstance}
-                <hr/>
+            <Grid>
                 <button onClick={this.getTweet}>getTweet</button>
-                {tweetList}
-            </div>
+                <ListGroup>
+                    {tweetListInstance}
+                </ListGroup>
+            </Grid>
         );
     }
 };
