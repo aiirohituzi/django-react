2097f632e2399ed090119765fc6cd0fb6f2600fa
aiirohituzi <sinn5656@gmail.com>
2019-09-24 23:06:38 +0900

    Album Repositories에서 만들어 놓은 django 서버로 이미지 출력도 테스트

--- testhooks/src/App.js
+++ testhooks/src/App.js
@@ -5,9 +5,15 @@ import useRequest from './useRequest';
 
 function App() {
   const [response, loading, error] = useRequest(
-    'http://127.0.0.1:8000/posting/'
+    'http://127.0.0.1:8000/photos/'
   );
   const [no, setNo] = useState(0);
+  
+  const [response2] = useRequest(
+    'http://127.0.0.1:8000/images/'
+  );
+  // console.log(response)
+  // console.log(response2)
   
   if (loading) {
     return <div>로딩중..</div>;
@@ -19,20 +25,30 @@ function App() {
 
   if (!response) return null;
 
-  const { id, title, content } = response.data[no];
+  const { title, content } = response.data[no];
+
+  let image = ''
+  if(response2 !== null){
+    for (let i = 0; i < response2.data.length; i++) {
+      if (response2.data[i].photoId === response.data[no].id) {
+        image = response2.data[i].image;
+      }
+    }
+  }
+  console.log('path:' + image);
 
   function click(op) {
     if (op === '+') {
-      if(no + 1 < response.data.length) {
+      if (no + 1 < response.data.length) {
         setNo(no + 1);
       } else {
-        console.log('Max range')
+        console.log('Max range');
       }
     } else if (op === '-') {
-      if(no - 1 >= 0) {
+      if (no - 1 >= 0) {
         setNo(no - 1);
       } else {
-        console.log('Min range')
+        console.log('Min range');
       }
     }
   }
@@ -40,7 +56,6 @@ function App() {
   return (
     <div>
       <h1>{title}</h1>
-      <p>{id}</p>
       <p>{content}</p>
       <button onClick={() => click('-')}>
         -
@@ -48,6 +63,7 @@ function App() {
       <button onClick={() => click('+')}>
         +
       </button>
+      <img src={'http://127.0.0.1:8000/media/photo/' + image}></img>
     </div>
   );
 }
